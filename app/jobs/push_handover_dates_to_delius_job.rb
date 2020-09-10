6d2f0a8e3d1bcf5cdf7ddd64f4348816c160cac1
Ollie Treend <ollie@treend.com>
2020-09-10 17:33:18 +0100

    Add a job to push handover dates to nDelius
    Using the Community API, this job will push the two handover dates for
    the offender into nDelius (handover start date, responsibility handover
    date).
    
    Queue jobs like so:
    
    ```
    PushHandoverDatesToDeliusJob.perform_later(nomis_offender_id)
    ```
    
    Handover dates with a `nil` value will be deleted from nDelius. This
    could happen if changes in an offender's case mean that OMIC rules
    no longer apply to them, and so handover is 'not applicable'.
    
    Non-nil handover dates will either be created or updated.

---
+++ app/jobs/push_handover_dates_to_delius_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class PushHandoverDatesToDeliusJob < ApplicationJob
+  queue_as :default
+
+  def perform(nomis_offender_id)
+    offender = OffenderService.get_offender(nomis_offender_id)
+
+    return if offender.nil?
+
+    HmppsApi::CommunityApi.set_handover_dates(
+      offender_no: offender.offender_no,
+      handover_start_date: offender.handover_start_date,
+      responsibility_handover_date: offender.responsibility_handover_date
+    )
+  end
+end
